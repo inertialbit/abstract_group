397c9a1969c3e450351238c3bfb83fe857b54e1e
Jeremiah Heller <jeremiah@inertialbit.net>
2010-09-16 09:20:57 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AbstractGroup::Application.config.secret_token = 'b8d20745aee1e985363132f72119c17fca829b8a144f309bac7858d946efa721b150daf2ecf4525e13880d14305fdd125007cb82de3cf1be239b58975139cc6a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AbstractGroup::Application.config.session_store :cookie_store, :key => '_abstract-group_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# AbstractGroup::Application.config.session_store :active_record_store
