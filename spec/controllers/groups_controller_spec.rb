36f17725409007e8a1f3723b0f0edee68e022646
Jeremiah Heller <jeremiah@inertialbit.net>
2010-09-16 10:39:14 -0700

    provide params as String rather than Fixnum

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -30,34 +30,34 @@ describe GroupsController do
   
   describe "GET :show, :id => integer" do
     before(:each) do
-      Group.stub(:find).with(1).and_return(mock_group)
+      Group.stub(:find).with("1").and_return(mock_group)
     end
     it "loads a group as @group" do
-      get :show, :id => 1
+      get :show, :id => "1"
       assigns[:group].should eql mock_group
     end
     it "renders the show template" do
-      get :show, :id => 1
+      get :show, :id => "1"
       response.should render_template("groups/show")
     end
   end
   
   describe "GET :edit, :id => integer" do
     before(:each) do
-      Group.stub(:find).with(1).and_return(mock_group)
+      Group.stub(:find).with("1").and_return(mock_group)
       Group.stub(:find).with(:all, :order => 'name').and_return([mock_group])
       Group.stub(:abstract_class?).and_return(false)
     end
     it "loads a group as @group" do
-      get :edit, :id => 1
+      get :edit, :id => "1"
       assigns[:group].should eql mock_group
     end
     it "loads groups as @groups" do
-      get :edit, :id => 1
+      get :edit, :id => "1"
       assigns[:groups].should eql [mock_group]
     end
     it "renders the edit template" do
-      get :edit, :id => 1
+      get :edit, :id => "1"
       response.should render_template("groups/edit")
     end
   end
@@ -108,27 +108,27 @@ describe GroupsController do
       mock_group({
         :update_attributes => nil
       })
-      Group.stub(:find).with(1).and_return(@group)
+      Group.stub(:find).with("1").and_return(@group)
       Group.stub(:find).with(:all, :order => 'name').and_return([@group])
     end
     it "loads a group as @group" do
-      put :update, :id => 1
+      put :update, :id => "1"
       assigns[:group].should eql mock_group
     end
     it "updates the group" do
       mock_group.should_receive(:update_attributes)
-      put :update, :id => 1
+      put :update, :id => "1"
     end
     context "update succeeds :)" do
       before(:each) do
         mock_group.stub(:update_attributes).and_return(true)
       end
       it "sets a flash[:notice]" do
-        put :update, :id => 1
+        put :update, :id => "1"
         flash[:notice].should_not be_nil
       end
       it "redirects to the group show page" do
-        put :update, :id => 1
+        put :update, :id => "1"
         response.should redirect_to group_path(mock_group)
       end
     end
@@ -137,11 +137,11 @@ describe GroupsController do
         mock_group.stub(:update_attributes).and_return(false)
       end
       it "loads groups as @groups" do
-        put :update, :id => 1
+        put :update, :id => "1"
         assigns[:groups].should eql [@group]
       end
       it "renders the edit template" do
-        put :update, :id => 1
+        put :update, :id => "1"
         response.should render_template("groups/edit")
       end
     end
@@ -153,14 +153,14 @@ describe GroupsController do
     end
     it "destroys a group" do
       Group.should_receive(:destroy)
-      delete :destroy, :id => 1
+      delete :destroy, :id => "1"
     end
     it "sets a flash[:notice]" do
-      delete :destroy, :id => 1
+      delete :destroy, :id => "1"
       flash[:notice].should_not be_nil
     end
     it "redirects to the group index page" do
-      delete :destroy, :id => 1
+      delete :destroy, :id => "1"
       response.should redirect_to groups_path
     end
   end
