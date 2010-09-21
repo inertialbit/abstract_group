1bd988e50d4056172fa4f2f03f154f4d1c61aa04
Jeremiah Heller <jeremiah@inertialbit.net>
2010-09-21 15:17:18 -0700

    declare gem 'formtastic' in engine init
    - provides more informative error if gem is not declared in integration app's Gemfile

--- lib/abstract-group/engine.rb
+++ lib/abstract-group/engine.rb
@@ -1,9 +1,7 @@
-# require 'abstract-group'
-# require 'rails'
-
 module AbstractGroup
   class Application < Rails::Engine
     engine_name :abstract_group
     config.action_controller.asset_path = "/abstract_group/%s"
+    config.gem 'formtastic'
   end
 end
