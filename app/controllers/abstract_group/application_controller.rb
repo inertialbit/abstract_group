6f7d3ff15a518c5cfd11f1db34c1f1fc56f7fc32
Jeremiah Heller <jeremiah@inertialbit.net>
2010-11-02 10:20:01 -0700

    removed engine_path et al. only useful when mounting engine routes
    - saved to http://gist.github.com/659638

--- app/controllers/abstract_group/application_controller.rb
+++ app/controllers/abstract_group/application_controller.rb
@@ -3,8 +3,6 @@ class AbstractGroup::ApplicationController < ApplicationController
 
   prepend_before_filter :use_engine_assets if Rails.env != 'production'
   prepend_before_filter :setup_asset_path if defined?(AbstractGroup::Engine)
-
-  helper_method :engine_path
   
   private
     def use_engine_assets
@@ -13,39 +11,6 @@ class AbstractGroup::ApplicationController < ApplicationController
     def setup_asset_path
       config.asset_path = '/abstract_group%s'
     end
-    def registered_engines
-      Rails::Engine.subclasses.map do |sc|
-        sc.to_s.underscore.gsub("/engine", "")
-      end << 'main_app'
-    end
-    def engine_path(obj_or_class, options={})
-      class_name = obj_or_class.kind_of?(Class) ? obj_or_class.to_s : obj_or_class.class.to_s
-
-      @cached_engine_classes ||= {}
-
-      return_path = ''
-      if @cached_engine_classes[class_name]
-        return_path = send(@cached_engine_classes[class_name]).polymorphic_path(obj_or_class, options)
-      end
-
-      count = 0
-      while return_path.blank? do
-        begin
-          if registered_engines[count].blank?
-            return_path = "/"
-          else
-            return_path = send(registered_engines[count]).polymorphic_path(obj_or_class, options)
-
-            @cached_engine_classes[class_name] = registered_engines[count]
-          end
-        rescue NoMethodError => e
-          logger.debug(e.message)
-          count += 1
-          retry
-        end
-      end
-      return_path
-    end    
   protected
   public
 end
